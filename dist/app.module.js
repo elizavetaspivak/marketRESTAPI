e88356a59b8fe36e4fc461049abca8ed547ef8ba
elizavetaspivak <elizabethspivak99@gmail.com>
2021-11-17 21:50:20 +0300

    change

--- dist/app.module.js
+++ dist/app.module.js
@@ -18,7 +18,7 @@ AppModule = __decorate([
     (0, common_1.Module)({
         imports: [
             products_module_1.ProductsModule,
-            mongoose_1.MongooseModule.forRoot(`mongodb+srv://liza:liza123@clusterformarket.d6zhs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`),
+            mongoose_1.MongooseModule.forRoot(`mongodb+srv://elizaveta:elizaveta@cluster0.r4c5f.mongodb.net/Cluster0?retryWrites=true&w=majority`),
         ],
         controllers: [app_controller_1.AppController],
         providers: [app_service_1.AppService],
--- src/app.module.ts
+++ src/app.module.ts
@@ -8,7 +8,7 @@ import { MongooseModule } from '@nestjs/mongoose';
   imports: [
     ProductsModule,
     MongooseModule.forRoot(
-      `mongodb+srv://liza:liza123@clusterformarket.d6zhs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
+      `mongodb+srv://elizaveta:elizaveta@cluster0.r4c5f.mongodb.net/Cluster0?retryWrites=true&w=majority`
     ),
   ],
   controllers: [AppController],
