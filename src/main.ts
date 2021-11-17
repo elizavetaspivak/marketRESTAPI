6c06320a7caf210857f62fdab62b92b68f4613dc
elizavetaspivak <elizabethspivak99@gmail.com>
2021-11-17 22:31:26 +0300

    change posrt

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT || 8080);
+  await app.listen(parseInt(`${process.env.PORT}`) || 8080, () => {
+    return 'Server started'
+  });
 }
 bootstrap();
