5dc3e0886eb24bacf2227e28359b7a7efda0334a
elizavetaspivak <elizabethspivak99@gmail.com>
2021-11-17 21:02:53 +0300

    change port

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  let PORT = process.env.PORT || 8080;
   const app = await NestFactory.create(AppModule);
-  await app.listen(PORT);
+  await app.listen(process.env.PORT || 8080);
 }
 bootstrap();
