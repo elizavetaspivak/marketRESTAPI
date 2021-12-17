e473458c12eeaff1f41ce3852c9f17e5d200bcfd
elizavetaspivak <elizabethspivak99@gmail.com>
2021-12-17 13:08:20 +0300

    add new endpoints

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,7 +2,7 @@ import {Module} from '@nestjs/common';
 import {AuthController} from "./auth.controller";
 import {AuthService} from "./auth.service";
 import {MongooseModule} from '@nestjs/mongoose';
-import {Auth, AuthSchema} from "./auth-schemas/auth.schema";
+import {Auth, AuthSchema} from "./schemas/auth.schema";
 
 @Module({
     providers: [AuthService],
--- src/review/review.module.ts
+++ src/review/review.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import {AuthService} from "../auth/auth.service";
 import {AuthController} from "../auth/auth.controller";
 import {MongooseModule} from "@nestjs/mongoose";
-import {Auth, AuthSchema} from "../auth/auth-schemas/auth.schema";
+import {Auth, AuthSchema} from "../auth/schemas/auth.schema";
 import {ReviewService} from "./review.service";
 import {ReviewController} from "./review.controller";
 import {Review, ReviewSchema} from "./schemas/review.schema";
